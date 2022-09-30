e774fc656f5fce94dfc348b0aa6247b6cbe6345f
Ghaleon <harakirighaleon@gmail.com>
2022-09-30 20:09:25 +0300

    added dog

---
+++ Examples/Example007_Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int distance = 10000;
+int FFS = 1; //FirstFriendSpeed
+int SFS = 2; //SecondFriendSpeed
+int DS = 5; //DogSpeed
+int FR = 2;
+int time = 0;
+int count = 0;
+while(distance > 10)
+{
+    if(FR == 1)
+    {
+        time = distance / (FFS + DS);
+        FR = 2;
+    }
+    else
+    {
+        time = distance / (SFS + DS);
+        FR = 1;
+    }
+     
+    distance = distance - (FFS + SFS) * time;
+
+    count++;
+}
+Console.WriteLine("dog run ");
+Console.Write(count);
