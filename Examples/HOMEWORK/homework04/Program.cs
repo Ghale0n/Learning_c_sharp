3753e30fe005b20511f140244426eb12fabb9b60
Ghaleon <harakirighaleon@gmail.com>
2022-10-03 00:48:42 +0300

    reshil04

--- Examples/HOMEWORK/homework04/Program.cs
+++ Examples/HOMEWORK/homework04/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вввод 3х чисел и вывод максимально
+
+Console.Clear();
+Console.WriteLine("Привет! Введи первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("А теперь второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ну и третье число: ");
+int z = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine($"в сумме это будет: {x + y + z}"); (Лирическое отступление)
+
+if (x > y)
+{
+    if (x > z)
+    {
+        Console.WriteLine($"Самое большое число - {x}");
+    }
+
+}
+else
+{
+    if (y > z)
+    {
+        Console.WriteLine($"Самое большое число - {y}");
+    }
+    else
+        Console.WriteLine($"Самое большое число - {z}");
+}
+
+// ИЗНАЧАЛЬНО ПЫТАЛСЯ СРАВНИТЬ КАЖДОЕ С КАЖДЫМ ЧЕРЕЗ ELSE IF, НО В ОТВЕТЕ ВСЕГДА ВЫДАВАЛО ПО ДВА ЧИСЛА ПОЧЕМУ-ТО
+//    else if (y > z) 
+//{
+//    Console.WriteLine($"Самое большое число - {x}");
+//}
+//if (y > z)
+//{
+//    Console.WriteLine($"Самое большое число - {y}");
+//}
+//    else if (z > x)
+//{
+//    Console.WriteLine($"Самое большое число - {y}");
+//}
+//if (z > y)
+//{
+//    Console.WriteLine($"Самое большое число - {z}");
+//}
+//    else if (y > x)
+//{
+//    Console.WriteLine($"Самое большое число - {z}");
+//}
