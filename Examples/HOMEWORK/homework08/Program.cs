63b6aaba5857afaca7b7255422d06c950cb8481b
Ghaleon <harakirighaleon@gmail.com>
2022-10-03 01:37:48 +0300

    reshil08YRA

---
+++ Examples/HOMEWORK/homework08/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача на вывод всех четных чисел от одного до n
+
+Console.Clear();
+Console.WriteLine("Привет! Крайняя задача на сегодня, ура! Введи число: ");
+int n = int.Parse(Console.ReadLine()!);
+int x = 1;
+int index = 1;
+while (index <= n)
+{
+    if (index % 2 == 0)
+    {
+        x = index;
+        Console.Write($"{x} ");
+    }
+    index = index + 1;
+}
+
