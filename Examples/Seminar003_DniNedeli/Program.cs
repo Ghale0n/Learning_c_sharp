e5f099119ea86adac1dd2eda22760267a98141c5
Ghaleon <harakirighaleon@gmail.com>
2022-10-02 14:27:58 +0300

    addNedelya

---
+++ Examples/Seminar003_DniNedeli/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пишем прогрраму, которая будет выдавать день недели по номеру
+
+Console.Clear();
+Console.WriteLine("Привет! Введи число от одного до семи: ");
+int x = int.Parse(Console.ReadLine()!);
+
+if (x == 1)
+{
+    Console.Write("Это понедельник!");
+}
+else if (x == 2)
+{
+    Console.Write("Это вторник!");
+}
+else if (x == 3)
+{
+    Console.Write("Это среда!");
+}
+else if (x == 4)
+    {
+        Console.Write("Это четвег!");
+    }
+else if (x == 5)
+    {
+        Console.Write("Это пятница!");
+    }
+else if (x == 6)
+    {
+        Console.Write("Это суббота!");
+    }
+else if (x == 7)
+    {
+        Console.Write("Это воскресенье!");
+    }
