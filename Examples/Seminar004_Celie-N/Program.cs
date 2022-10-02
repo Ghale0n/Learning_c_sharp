ed4fd97c9767a32188501b735c752e214f2f49ac
Ghaleon <harakirighaleon@gmail.com>
2022-10-02 15:45:43 +0300

    addCelie

---
+++ Examples/Seminar004_Celie-N/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает число N и показывает все целые числа в промежутке от -N до N
+
+//Сonsole.Clear();
+Console.WriteLine("Привет! Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int X = (N * -1);
+int index = X;
+Console.Write(X);
+while (index < N)
+{
+    X = X + 1;
+    Console.Write(X);
+    index++;
+}
