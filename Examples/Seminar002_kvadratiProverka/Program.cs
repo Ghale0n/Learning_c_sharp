0736c069940f31a54d478d187301dbb397e6ece5
Ghaleon <harakirighaleon@gmail.com>
2022-10-02 14:09:48 +0300

    addkvproverka

---
+++ Examples/Seminar002_kvadratiProverka/Program.cs
@@ -0,0 +1,20 @@
+﻿// напиши программу, которая принимает на вход 2 числа и проверяет является ли одно число квадратом второго
+
+Console.Clear();
+Console.WriteLine("Привет! Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (y * y == x)
+{
+    Console.WriteLine("Да, это квадрат!");
+}
+else
+{
+    Console.WriteLine("Нет, это ошибка");
+}
+
+
+
+
