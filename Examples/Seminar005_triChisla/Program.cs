a8971bde29d5d7cfcce8ea6071b2f44e8424dd0b
Ghaleon <harakirighaleon@gmail.com>
2022-10-02 16:22:26 +0300

    addTrichisla

---
+++ Examples/Seminar005_triChisla/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа принимает на вход 3 числа и показывает последюю цифру числа
+
+Console.Clear();
+Console.WriteLine("Привет! Введите трехзначное число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{x%10}");
