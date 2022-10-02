f0e955a0089a35d2db56da0672d5681b7504aca3
Ghaleon <harakirighaleon@gmail.com>
2022-10-03 01:03:10 +0300

    reshil06

---
+++ Examples/HOMEWORK/homework06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача на проверку четности вводимого числа
+
+Console.Clear();
+Console.WriteLine("Привет! Введи число: ");
+int x = int.Parse(Console.ReadLine()!);
+if (x % 2 == 0)
+{
+    Console.Write("Ура, число четное");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
