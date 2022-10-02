cffbe0c228f01fdf38aaf37f5896216b2126f1f3
Ghaleon <harakirighaleon@gmail.com>
2022-10-02 13:40:37 +0300

    add kvadrat

---
+++ Examples/Seminar001_KvadratChisla/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу, которая на вход принимает число
+// и выдаёт его квардрат (число, умноженное само на себя)
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
