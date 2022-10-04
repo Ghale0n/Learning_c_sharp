5194b87dc1f09ecfda26f67fa761d3c6609fa491
Ghaleon <harakirighaleon@gmail.com>
2022-10-04 16:02:29 +0300

    addarray2

---
+++ Examples/Example010_nomerArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 28, 33, 24, 55, 64, 37, 28 };
+
+int n = array.Length;
+int find = 28;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
