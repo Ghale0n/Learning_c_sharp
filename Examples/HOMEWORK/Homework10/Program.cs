d09c40b78aff6746787a495d0aaf0779dd402375
Ghaleon <harakirighaleon@gmail.com>
2022-10-05 20:15:19 +0300

    reshilzadachy10

---
+++ Examples/HOMEWORK/Homework10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Прога на ввод трехзначного числа, а на выходе показывает второе число
+
+Console.Clear();
+Console.WriteLine("Привет! Введите трехзначное число: ");
+int x = int.Parse(Console.ReadLine()!);
+int first = x / 100; //первая цифра числа без остатка
+first = first * 100;
+int third = x%10;
+int result1 = (x - first - third)/10;
+
+Console.WriteLine($"Второе число из трехзначного - {result1}");
