e21dc1968f9fdafc1176ecb89e7538b70ecbecbc
Ghaleon <harakirighaleon@gmail.com>
2022-10-06 14:09:55 +0300

    reshil13

--- Examples/HOMEWORK/homework13/Program.cs
+++ Examples/HOMEWORK/homework13/Program.cs
@@ -4,6 +4,34 @@ Console.Clear();
 Console.WriteLine("Привет! Введите число: ");
 int x = int.Parse(Console.ReadLine()!);
 
+/*int res1 = x % 10; // последняя цифра числа
+int res2 = x / 10; //результат целочисленного деления на 10
+
+Console.WriteLine($"последняя цифра числа- {res1}");
+Console.WriteLine($"результат целочисленного деления на 10 - {res2}");
+*/
+
+while (x > 0)
+{
+    if (x <= 99)
+    {
+        Console.WriteLine("Нет третьего числа");
+        break;
+    }
+    if (x <= 999)
+    {
+        x = x % 10;
+        Console.WriteLine($"Третье по счету число: {x}");
+        break;
+    }
+    x = x / 10;
+
+
+
+}
+
+
+
 // Тут я переусложнил себе задачу, решив, что по дано пользователь должен задавать индекс искомого числа
 
 // Console.WriteLine("Привет! Введите индекс числа: "); 
@@ -12,13 +40,13 @@ int x = int.Parse(Console.ReadLine()!);
 // int[] array = new int[x];
 // int n = array.Length;
 // index = n;
-
+/*int index = 10;
 while (x >= x % 100)
 {
-    x / 10;
-    Console.Write("111");
-    break;
-}
+x = x / index;
+Console.Write("делим");
+//break;
+
 
 if (x >= 100)
 {
@@ -29,5 +57,7 @@ if (x < 100)
 {
     Console.WriteLine("Нет третьего числа");
 }
-
+index++;
+}
 // Console.WriteLine($"третья цифра числа {x} - это {result} !");
+*/
