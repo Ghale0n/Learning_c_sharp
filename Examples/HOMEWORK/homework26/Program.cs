2e239409d55eb5045aaa1f0af22fcef0d2f17e04
Ghaleon <harakirighaleon@gmail.com>
2022-10-14 01:24:56 +0300

    reshil26

---
+++ Examples/HOMEWORK/homework26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int Stepen(int x, int step)
+
+{
+    
+    int pro = x;
+    for (int i = 1; i < step; i++)
+    {
+        pro *= x;
+    }
+    return pro;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число А: ");
+int q = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B: ");
+int w = int.Parse(Console.ReadLine()!);
+
+int result = Stepen(q, w);
+Console.Write($"Число А в степени В = {result}");
