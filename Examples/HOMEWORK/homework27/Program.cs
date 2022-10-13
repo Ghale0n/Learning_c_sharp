21d8d42ff4e19f4101fabd87707bfbf9f1339ced
Ghaleon <harakirighaleon@gmail.com>
2022-10-14 01:38:15 +0300

    dobavil27

---
+++ Examples/HOMEWORK/homework27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int SummCifr(int x)
+{
+    int count = 0;
+    for (int i = 0; x > 0; i++)
+    {
+        x = x%10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число: ");
+int w = int.Parse(Console.ReadLine()!);
+
+int result = SummCifr(w);
+Console.Write($"Число А в степени В = {result}");
