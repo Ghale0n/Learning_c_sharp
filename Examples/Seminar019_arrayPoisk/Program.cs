3a1d7e846ebce755278b2bb8166090fbe01aadac
Ghaleon <harakirighaleon@gmail.com>
2022-10-16 02:16:30 +0300

    seminar4

---
+++ Examples/Seminar019_arrayPoisk/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] getArray(int size, int minValue, int maxValue) // функция задаёт массив со случайными числами 
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void ArrayMinus(int[] array) // умножнает массив на -1
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+
+Console.Clear();
+
+int[] array = getArray(5, -100, 100);
+Console.WriteLine(String.Join(", ", array));
+
+ArrayMinus(array);
+Console.WriteLine(String.Join(", ", array));
+
+
---
+++ Examples/Seminar020_arrayPoisk2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] GetArrayFromString(string stringArray) //позволяет считывать и запоминать элементы из строки в массив
+{
+    string[] numbers = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[numbers.Length];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        res[i] = int.Parse(numbers[i]);
+    }
+    return res;
+}
+
+bool CheckElement(int[] array, int num) // проверяет есть ли элемент в массиве
+{
+    foreach (int el in array)
+    {
+        if (el == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+Console.Clear();
+Console.WriteLine("Введите числа для массива через пробел: ");
+string input = Console.ReadLine();
+int[] arr = GetArrayFromString(input);
+
+Console.WriteLine("Введите число для проверки массива: ");
+int n = int.Parse(Console.ReadLine()!); 
+
+
+if (CheckElement(arr, n))
+{
+    Console.Write("Есть такое");
+
+}
+else
+{
+    Console.Write("Нет такого");
+}
