9d6c0b1dd312f3beee7382d9e617e5d9dbe09ec6
Ghaleon <harakirighaleon@gmail.com>
2022-10-16 16:51:07 +0300

    Seminar5

---
+++ Examples/Seminar021_zadacha35/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 
+
+int[] getArray(int size, int minValue, int maxValue) // функция задаёт массив со случайными числами 
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+int SummChisla(int[] numArray) // считает сколько чисел лежит в заданной области
+{
+    int count = 0;
+    for (int i = 0; i < numArray.Length; i++)
+    {
+        if(numArray[i] > 9 && numArray[i] < 100)
+        {
+            count +=1;
+        }
+    }
+    return count;
+}
+
+
+Console.Clear();
+int[] arr = getArray(123, -100, 100);
+Console.Write($"{SummChisla(arr)}");
---
+++ Examples/Seminar022_zadacha37/Program.cs
@@ -0,0 +1,48 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] GetArrayFromString(string stringArray) //позволяет считывать и запоминать элементы из строки в массив
+{
+    string[] numbers = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[numbers.Length];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        res[i] = int.Parse(numbers[i]);
+    }
+    return res;
+}
+
+int[] ResultArray(int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 != 0)
+    {
+        size++;
+    }
+
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (array.Length % 2 != 0)
+    {
+        result[size - 1] = array[array.Length / 2];
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите числа для массива через пробел: ");
+string input = Console.ReadLine();
+int[] numArray = GetArrayFromString(input);
+
+int[] newArray = ResultArray(numArray);
+
+//Console.WriteLine($"Произведение пар массива {numArray}: {newArray}"!);
+
+Console.WriteLine(String.Join(", ", newArray));
