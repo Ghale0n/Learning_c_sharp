4e55abdd1d174ad447003775f6f18e58f44c2e08
Ghaleon <harakirighaleon@gmail.com>
2022-10-25 16:53:27 +0300

    reshil41

--- Examples/HOMEWORK/homework41/Program.cs
+++ Examples/HOMEWORK/homework41/Program.cs
@@ -4,3 +4,38 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+int[] GetArrayFromString(string stringArray) //позволяет считывать и запоминать элементы из строки в массив
+{
+    string[] numbers = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[numbers.Length];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        res[i] = int.Parse(numbers[i]);
+    }
+    return res;
+}
+
+int SummPositive (int[] array)
+{
+    int summ = 0;
+    foreach (int el in array)
+    {
+        if (el > 0)
+        {
+            summ ++;
+        }
+    }
+    return summ;
+}
+
+Console.Clear();
+
+Console.WriteLine("Введите ряд чисел через пробел: ");
+string x = Console.ReadLine()!;
+
+int[] result = GetArrayFromString(x);
+
+int Summ = SummPositive(result);
+
+Console.WriteLine($"Количество чисел > 0: {Summ}");
