4139954e9dae1f1c145c5c9496f80ff45260e9db
Ghaleon <harakirighaleon@gmail.com>
2022-10-28 16:30:54 +0300

    reshil50spodskazkoy

---
+++ Examples/HOMEWORK/homework50/Program.cs
@@ -0,0 +1,67 @@
+﻿// Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int[,] Get2Array(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+
+void Print2Array(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void PoiskIn2Array(int[,] array, int poisk)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == poisk)
+            {
+                Console.WriteLine($"число {poisk} присутствует в массиве в столбце {j + 1}, строке {i + 1}");
+                return;
+            }
+        }
+    }
+    Console.WriteLine($"{poisk} - число в массиве не встречается");
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число, которое хотите найти от 0 до 40: ");
+int number = int.Parse(Console.ReadLine()!);
+
+
+
+int[,] myArray = Get2Array(rows, columns, 0, 40);
+Print2Array(myArray);
+PoiskIn2Array(myArray, number);
