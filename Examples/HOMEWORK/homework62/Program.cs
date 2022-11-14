d9c8ce5be13a2eb9c95c335a6be965ac3b33cd4c
Ghaleon <harakirighaleon@gmail.com>
2022-11-15 00:48:11 +0300

    podsmotrel62

---
+++ Examples/HOMEWORK/homework62/Program.cs
@@ -0,0 +1,37 @@
+﻿// Заполните спирально массив 4 на 4.
+
+int[,] ArrangeQuadroArray()
+{
+    int s = 4;
+    int[,] array = new int[s, s];
+    int i = 0; 
+    int j = 0;
+    int num = 1;
+
+    for (int n = 1; n < 16; n++)
+    {
+        int k = 0;
+        for (k = 0; k < s - 1; k++) array[i, j++] = num++;
+        for (k = 0; k < s - 1; k++) array[i++, j] = num++;
+        for (k = 0; k < s - 1; k++) array[i, j--] = num++;
+        for (k = 0; k < s - 1; k++) array[i--, j] = num++;
+        ++i; 
+        ++j;
+        s = s / 2;
+    }
+    return array;
+}
+
+void Print2Array(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Print2Array(ArrangeQuadroArray());
