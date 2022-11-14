9f571d58919e5d5d490a02031b34961da8faa610
Ghaleon <harakirighaleon@gmail.com>
2022-11-14 23:24:49 +0300

    reshil58

---
+++ Examples/HOMEWORK/homework58/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] Get2Array(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+
+void Print2Array(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiplyMatrix(int[,] firstArray, int[,] secondArray)
+{
+    int[,] resultArray = new int[firstArray.GetLength(0), secondArray.GetLength(1)];
+
+    for (int row = 0; row < resultArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < resultArray.GetLength(1); column++)
+        {
+            int sum = 0;
+            for (int i = 0; i < firstArray.GetLength(1); i++)
+            {
+                sum += firstArray[row, i] * secondArray[i, column];
+            }
+            resultArray[row, column] = sum;
+        }
+    }
+    Console.WriteLine("произведение двух матриц:");
+    return resultArray;
+}
+
+
+Console.Clear();
+Console.WriteLine("Даны две матрицы 2*2:");
+int[,] array1 = Get2Array(2,2,0,10);
+int[,] array2 = Get2Array(2,2,0,10);
+
+Print2Array(array1);
+Console.WriteLine();
+Print2Array(array2);
+Console.WriteLine();
+Print2Array(MultiplyMatrix(array1, array2));
